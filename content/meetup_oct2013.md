f70b2ce30be33b3b403d6f50e285fbe2f31e3510
Marconi Moreto <caketoad@gmail.com>
2013-10-21 20:24:25 +0800

    initial commit

---
+++ content/meetup_oct2013.md
@@ -0,0 +1,12 @@
+Title: October 2013 Meetup
+Date: 2013-10-21 08:00
+Category: Meetups
+Slug: october-2013-meetup
+Author: Marconi Moreto
+
+**What**: Meetup  
+**When**: 1383001200  
+**Where**: Function Room, Handuraw Pizza, Gorordo St., Cebu City, PH  
+**Talks**: ?
+
+We are still finalising the talks so please visit this page soon.
---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Marconi Moreto'
+SITENAME = u'Pizzapy'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Hong_Kong'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
